d1ed4af399cb80e3df1a4b5266b96cd586e754a5
anon <moremike@protonmail.com>
2019-09-28 14:08:42 -0600

    Initial Commit

---
+++ Nip/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nip
+//
+//  Created by Public on 9/28/19.
+//  Copyright © 2019 Public. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
